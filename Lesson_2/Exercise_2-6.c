50d445b45907fddd10c3f5594261994ac53bfbcf
manderson <anderson.c.matt@gmail.com>
2021-04-01 11:21:08 -0500

    Add Exercise_2-6.c Write a function setBits(x,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged

---
+++ Lesson_2/Exercise_2-6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Write a function setBits(x,p,n,y) that returns x with the n bits that
+// begin at position p set to the rightmost n bits of y, leaving other bits unchanged
+
+unsigned setBits(unsigned x, int p, int n, unsigned y);
+
+int main() {
+
+    printf("%u", setBits(31, 3, 3, 2));
+
+    return 0;
+}
+
+unsigned setBits(unsigned x, int p, int n, unsigned y) {
+
+    // Creates a mask of n digits turned on
+    int mask = ~(~0 << n);
+    // Amount of bits to shift to p
+    int shift = (p+1-n);
+
+    // Bits to be changed are turned off in x
+    // Bits to be changed are turned on in y
+    // OR then creates a new value with the changes in y in the position p
+    return (x & ~(mask << shift)) | (y & (mask << shift));
+}
